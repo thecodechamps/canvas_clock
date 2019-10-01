451a241fcaaaff80eb7b0c9ebf0df44200ee037d
vedhant <vedhantagarwal2@gmail.com>
2019-10-01 11:23:44 +0530

    Use ctx.rotate instead of taking components

--- draw.js
+++ draw.js
@@ -18,50 +18,54 @@ Clock.prototype.draw = function(cx, cy) {
     ctx.stroke();
 
     for(let i=0; i<12; ++i) {
-        let angle = (i / 12) * Math.PI * 2 - Math.PI / 3;
         ctx.beginPath();
-        let nx = this.radius * Math.cos(angle);
-        let ny = this.radius * Math.sin(angle);
-        ctx.moveTo(nx, ny);
+        ctx.rotate(Math.PI / 6);
+        ctx.moveTo(0, -this.radius);
         ctx.lineWidth = 3;
-        ctx.lineTo(nx * 0.96, ny * 0.96);
+        ctx.lineTo(0, -this.radius * 0.96);
         ctx.stroke();
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
         ctx.font = this.radius * 0.08 + 'px Courier New';
         ctx.fillStyle = "#0E2A38";
-        ctx.fillText(i+1, nx * 0.9, ny * 0.9);
+        ctx.fillText(i+1, 0, -this.radius * 0.9);
     }
 
-    let sec_angle = (this.seconds / 60) * Math.PI * 2 - Math.PI / 2;
+    let sec_angle = (this.seconds / 60) * Math.PI * 2;
     ctx.beginPath();
+    ctx.rotate(sec_angle);
     ctx.moveTo(0, 0);
-    ctx.lineTo(this.radius * Math.cos(sec_angle) * 0.9, this.radius * Math.sin(sec_angle) * 0.9);
+    ctx.lineTo(0, -this.radius * 0.9);
     ctx.strokeStyle = "#D26363";
     ctx.lineWidth = 3;
     ctx.stroke();
     ctx.beginPath();
     ctx.moveTo(0, 0);
-    ctx.lineTo( - this.radius * Math.cos(sec_angle) * 0.1, - this.radius * Math.sin(sec_angle) * 0.1);
+    ctx.lineTo(0, this.radius * 0.15);
     ctx.lineWidth = 6;
     ctx.stroke();
+    ctx.rotate(-sec_angle);
 
-    let min_angle = ((this.minutes + this.seconds / 60) / 60) * Math.PI * 2 - Math.PI / 2;
+    let min_angle = ((this.minutes + this.seconds / 60) / 60) * Math.PI * 2;
     ctx.beginPath();
+    ctx.rotate(min_angle);
     ctx.moveTo(0, 0);
-    ctx.lineTo(this.radius * Math.cos(min_angle) * 0.7, this.radius * Math.sin(min_angle) * 0.7);
+    ctx.lineTo(0, -this.radius * 0.7);
     ctx.strokeStyle = "#304753";
     ctx.lineWidth = 6;
     ctx.lineCap = "round";
     ctx.stroke();
+    ctx.rotate(-min_angle);
 
-    let hour_angle = ((this.hours + this.minutes / 60) / 12) * Math.PI * 2 - Math.PI / 2;
+    let hour_angle = ((this.hours + this.minutes / 60) / 12) * Math.PI * 2;
     ctx.beginPath();
+    ctx.rotate(hour_angle);
     ctx.moveTo(0, 0);
-    ctx.lineTo(this.radius * Math.cos(hour_angle) * 0.4, this.radius * Math.sin(hour_angle) * 0.4);
+    ctx.lineTo(0, -this.radius * 0.4);
     ctx.strokeStyle = "#304753";
     ctx.lineWidth = 12;
     ctx.stroke();
+    ctx.rotate(-hour_angle);
 
     ctx.beginPath();
     ctx.arc(0, 0, this.radius / 20, 0, Math.PI * 2);
